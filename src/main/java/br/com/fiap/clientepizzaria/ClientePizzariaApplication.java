61fa6c0ca088822e10fad3a21f0570005be3022a
andresant-ana <rm551575@fiap.com.br>
2024-02-27 09:47:35 -0300

    Criação da tabela de cliente da pizzaria

--- src/main/java/br/com/fiap/clientepizzaria/ClientePizzariaApplication.java
+++ src/main/java/br/com/fiap/clientepizzaria/ClientePizzariaApplication.java
@@ -2,12 +2,26 @@ package br.com.fiap.clientepizzaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ClientePizzariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePizzariaApplication.class, args);
+	}
+
+	@GetMapping(value = "/")
+	public String index() {
+		String hello = """
+							Bom dia!
+							
+							Seja bem-vindo(a) à Pizzaria!
+				""";
+
+		return hello;
 	}
 
 }
